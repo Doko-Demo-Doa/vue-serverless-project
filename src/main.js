37bbd01a31a8d9727bf66b351bc5cd8540e48ae5
Quan Pham <doraemonfanclub@gmail.com>
2018-10-04 01:33:49 +0700

    Added Amplify Plugin Vue into code

--- src/main.js
+++ src/main.js
@@ -1,3 +1,11 @@
+import Amplify, * as AmplifyModules from 'aws-amplify'
+import { AmplifyPlugin } from 'aws-amplify-vue'
+import aws_exports from './aws-exports';
+
+Amplify.configure(aws_exports);
+
+Vue.use(AmplifyPlugin, AmplifyModules)
+
 import Vue from 'vue'
 import App from './App.vue'
 
